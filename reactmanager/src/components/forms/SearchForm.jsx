3c318ff7f8df13db83b83041f40c8e8638a15f3f
axelthereal <mail.axeltokam@gmail.com>
2024-07-05 11:49:53 +0100

    added searchform to homeview

---
+++ reactmanager/src/components/forms/SearchForm.jsx
@@ -0,0 +1,5 @@
+const SearchForm = () => {
+  return <></>;
+};
+
+export default SearchForm;
--- reactmanager/src/views/HomeView.tsx
+++ reactmanager/src/views/HomeView.tsx
@@ -1,11 +1,15 @@
 import AppCard from "../components/cards/AppCard";
 import Navbar from "../components/headers/Navbar";
+import SearchForm from "../components/forms/SearchForm";
 
 const HomeView = () => {
   return (
     <>
       <AppCard>
         <Navbar />
+        <div>
+          <SearchForm />
+        </div>
         <div className="body container px-3">
           <h1>Home</h1>
         </div>
